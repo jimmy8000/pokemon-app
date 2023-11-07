d9c2b629a90f53c7a845351338757cf8707e615f
Munkhjargal Batsaikhan <100424489+jimmy8000@users.noreply.github.com>
2023-11-07 11:01:40 -0600

    Update scripts.js
    added pokemon list array

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,17 @@
-alert('Hello world');
-
-let favoriteFood = 'Pasta';
-
-document.write(favoriteFood);
+let pokemonList = [
+    {
+        name: 'pikachu', 
+        height: 0.4,
+        abilities: ['static', 'lightningrod']
+    },
+    {
+        name: 'clefable',
+        height: 1.3,
+        abilities: ['cute-charm', 'magic-guard', 'unaware']
+    },
+    {
+        name: 'blastoise',
+        height: 1.6,
+        abilities: ['rain-dish', 'torrent']
+    }
+];
