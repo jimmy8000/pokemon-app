f8df65bc060d75191cec2be81c9bc9a5f3e9a551
Munkhjargal Batsaikhan <100424489+jimmy8000@users.noreply.github.com>
2023-11-07 14:05:11 -0600

    added a for loop
    added a for loop and overwrote the document with the loop. and added some basic stylings.

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,28 @@
 let pokemonList = [
     {
-        name: 'pikachu', 
+        name: 'Pikachu', 
         height: 0.4,
         abilities: ['static', 'lightningrod']
     },
     {
-        name: 'clefable',
+        name: 'Clefable',
         height: 1.3,
         abilities: ['cute-charm', 'magic-guard', 'unaware']
     },
     {
-        name: 'blastoise',
+        name: 'Blastoise',
         height: 1.6,
         abilities: ['rain-dish', 'torrent']
     }
 ];
+
+for (let i = 0; i < pokemonList.length; i++) {
+    if (pokemonList[i].height > 1.5) {
+        document.write(`${pokemonList[i].name} (height: ${pokemonList[i].height}) Wow that's big <br>`)
+    } else if (pokemonList[i].height < 0.5) {
+        document.write(`${pokemonList[i].name} (height: ${pokemonList[i].height}) He's a small guy <br>`)
+    }
+    else {
+    document.write(`${pokemonList[i].name} (height: ${pokemonList[i].height}) He's perfectly ordinary <br>`)
+    }
+}
