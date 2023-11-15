66ddb60b2f9b3ab211f14279d04b4fe17914e519
Munkhjargal Batsaikhan <100424489+jimmy8000@users.noreply.github.com>
2023-11-15 10:54:45 -0600

    1.5: JavaScript Functions Part 2
    added forEach loop and IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,43 @@
-let pokemonList = [
+let pokemonRepository = (function () {
+  let pokemonList = [
     {
-        name: 'Pikachu', 
-        height: 0.4,
-        abilities: ['static', 'lightningrod']
+      name: "Pikachu",
+      height: 0.4,
+      abilities: ["static", "lightningrod"],
     },
     {
-        name: 'Clefable',
-        height: 1.3,
-        abilities: ['cute-charm', 'magic-guard', 'unaware']
+      name: "Clefable",
+      height: 1.3,
+      abilities: ["cute-charm", "magic-guard", "unaware"],
     },
     {
-        name: 'Blastoise',
-        height: 1.6,
-        abilities: ['rain-dish', 'torrent']
+      name: "Blastoise",
+      height: 1.6,
+      abilities: ["rain-dish", "torrent"],
+    },
+  ];
+  return {
+    getAll: function () {
+        return pokemonList;
+    },
+    add: function (pokemon) {
+        pokemonList.push(pokemon);
     }
-];
+  }
+})();
 
-for (let i = 0; i < pokemonList.length; i++) {
-    if (pokemonList[i].height > 1.5) {
-        document.write(`${pokemonList[i].name} (height: ${pokemonList[i].height}) Wow that's big <br>`)
-    } else if (pokemonList[i].height < 0.5) {
-        document.write(`${pokemonList[i].name} (height: ${pokemonList[i].height}) He's a small guy <br>`)
+pokemonRepository.getAll().forEach(function (pokemon) {
+    if (pokemon.height > 1.5) {
+      document.write(
+        `${pokemon.name} (height: ${pokemon.height}) Wow that's big <br>`
+      );
+    } else if (pokemon.height < 0.5) {
+      document.write(
+        `${pokemon.name} (height: ${pokemon.height}) He's a small guy <br>`
+      );
+    } else {
+      document.write(
+        `${pokemon.name} (height: ${pokemon.height}) He's perfectly ordinary <br>`
+      );
     }
-    else {
-    document.write(`${pokemonList[i].name} (height: ${pokemonList[i].height}) He's perfectly ordinary <br>`)
-    }
-}
+  });
